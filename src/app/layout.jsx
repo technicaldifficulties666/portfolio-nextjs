587691a8f971e8b0e79968a9f76e9a0a53254785
Subaig Bindra <subaig_singh_bindra@sfu.ca>
2024-08-22 12:53:29 -0700

    added vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,12 @@
 import Favicon from "/public/images/favicon1.png";
 import ClientRootLayout from "../components/ClientRootLayout";
 import Footer from "../components/Footer";
+import {Analytics} from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Subaig Bindra",
   description:
-  "Welcome to Subaig's Portfolio. Explore a diverse range of projects and see how I can bring your ideas to life. Let's connect and discuss your next venture!",
+    "Welcome to Subaig's Portfolio. Explore a diverse range of projects and see how I can bring your ideas to life. Let's connect and discuss your next venture!",
   keywords: [
     "portfolio",
     "app",
@@ -16,14 +17,21 @@ export const metadata = {
     "chakra",
     "hashnode",
     "best portfolio",
-    "best portfolio website"
+    "best portfolio website",
   ],
-    icons: [{
-    rel: 'icon',
-    url: Favicon.src,
-  }]
+  icons: [
+    {
+      rel: "icon",
+      url: Favicon.src,
+    },
+  ],
 };
 
-export default function RootLayout({ children }) {
-  return <ClientRootLayout>{children}</ClientRootLayout>
+export default function RootLayout({children}) {
+  return (
+    <ClientRootLayout>
+      {children}
+      <Analytics />
+    </ClientRootLayout>
+  );
 }
